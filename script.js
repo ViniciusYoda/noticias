0b4034e387b2f97364962aaca8416d426f4ddba0
Vinícius Yoda <90533635+ViniciusYoda@users.noreply.github.com>
2023-10-15 15:02:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,18 +5,23 @@ async function getNews() {
     try {
         const response = await fetch(`https://newsapi.org/v2/top-headlines?country=br&apiKey=${apiKey}`);
         const data = await response.json();
-        const articles = data.articles;
 
-        articles.forEach(article => {
-            const newsItem = document.createElement('div');
-            newsItem.classList.add('news-item');
-            newsItem.innerHTML = `
-                <h2>${article.title}</h2>
-                <p>${article.description || 'Descrição não disponível'}</p>
-                <a href="${article.url}" target="_blank">Leia mais</a>
-            `;
-            newsContainer.appendChild(newsItem);
-        });
+        if (data.status === 'ok') {
+            const articles = data.articles;
+
+            articles.forEach(article => {
+                const newsItem = document.createElement('div');
+                newsItem.classList.add('news-item');
+                newsItem.innerHTML = `
+                    <h2>${article.title}</h2>
+                    <p>${article.description || 'Descrição não disponível'}</p>
+                    <a href="${article.url}" target="_blank">Leia mais</a>
+                `;
+                newsContainer.appendChild(newsItem);
+            });
+        } else {
+            console.error('Erro ao buscar notícias: ', data.message);
+        }
     } catch (error) {
         console.error('Erro ao buscar notícias:', error);
     }
